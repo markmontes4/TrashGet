f2490221c511285a8efcca6d06ffe30e4449d20f
Mark Montes <markmontes@Mark-Montes-MacBook-Pro-2.local>
2022-12-22 17:16:37 -0800

    get route works

--- routes/gets.js
+++ routes/gets.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../assets/models/target');
 
-router.get('/', (req, res) => {
-  res.send('Get')
+router.get('/', async (req, res) => {
+  try{
+    const posts = await Post.find();
+    res.json(posts);
+  }catch(err){
+    res.json({message: err});
+  }
 });
 
 module.exports = router;
