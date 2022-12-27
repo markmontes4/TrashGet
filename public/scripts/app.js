cbf8e80aeb109f3157be21a8e549602e21a3b527
markmontes4 <mark.montes44@gmail.com>
2022-12-27 07:21:22 -0800

    added modal functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 var map = L.map('map').setView([45.5152, -122.6784], 10);
 var myPos = [];
+var markers = {};
 var targetCount = 0;
 var myIcon = L.icon({
   iconUrl: 'imgs/arms-up.png',
@@ -28,7 +29,52 @@ async function getAllCoords(e){
     const json = await res.json();
     console.log(json);
     json.forEach((target) => {
-        L.marker(target.coords, {icon: trashIcon}).addTo(map);
+        var modalNum = targetCount;
+        marker = L.marker(target.coords, {icon: trashIcon, title: target.title}).addTo(map);
+        modal = document.createElement('div');
+          modal.innerHTML = 
+            '<div class="portfolio-modal modal fade" id="portfolioModal' + targetCount + '" tabindex="-1" aria-labelledby="portfolioModal' + targetCount + '" aria-hidden="true"> ' +
+            '<div class="modal-dialog modal-xl"> ' +
+                '<div class="modal-content"> ' +
+                    '<div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div> ' +
+                    '<div class="modal-body text-center pb-5"> ' +
+                        '<div class="container"> ' +
+                            '<div class="row justify-content-center"> ' +
+                                '<div class="col-lg-8"> ' +
+                                    '<!-- Portfolio Modal - Title--> ' +
+                                    '<h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">' + target.title + '</h2> ' +
+                                    '<!-- Icon Divider--> ' +
+                                    '<div class="divider-custom"> ' +
+                                        '<div class="divider-custom-line"></div> ' +
+                                        '<div class="divider-custom-icon"><i class="fas fa-star"></i></div> ' +
+                                        '<div class="divider-custom-line"></div> ' +
+                                    '</div> ' +
+                                    '<!-- Portfolio Modal - Image--> ' +
+                                    '<img class="img-fluid rounded mb-5" src="imgs/bridge-garbage.jpg" alt="..." /> ' +
+                                    '<!-- Portfolio Modal - Text--> ' +
+                                    '<p class="mb-4">' + target.description + '</p> ' +
+                                    '<button class="btn btn-primary" data-bs-dismiss="modal"> ' +
+                                        '<i class="fas fa-xmark fa-fw"></i> '+
+                                        'Close Window ' +
+                                    '</button> ' +
+                                '</div> ' +
+                            '</div> ' +
+                        '</div> ' +
+                    '</div> ' +
+                '</div> ' +
+            '</div> ' +
+        '</div>';
+        // console.log(html);
+        document.body.append(modal);
+        marker.addEventListener("click", (e) => {
+          console.log(modalNum)
+          const currModal = "#portfolioModal" + modalNum;
+          $(currModal).modal('show');
+        });
+       
+        
+        targetCount = targetCount + 1;
+        markers[target.title] = marker
     })
   } catch(err){
       console.log({error: err});
@@ -48,8 +94,8 @@ if ('geolocation' in navigator) {
 }
 
 map.addEventListener("click", (e) => {
-  $('#portfolioModal1').modal('show');
-  console.log("modal closed");
+  // $('#portfolioModal1').modal('show');
+  console.log(targetCount);
   // L.marker(e.latlng, {icon: trashIcon}).addTo(map);
   // console.log(e.latlng);
   // const options = {
@@ -61,3 +107,11 @@ map.addEventListener("click", (e) => {
   // }
   // fetch('/post', options);
 });
+
+function markerClick(modal){
+
+}
+
+function testButton(){
+  console.log(markers);
+}
