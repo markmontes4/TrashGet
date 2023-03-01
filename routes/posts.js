d4c6f6969e4ff94175f5831fc71f6ce1aa899d78
dev <dev@DevMmontes.local>
2023-02-28 21:28:12 -0800

    added local storage for images feature

--- routes/posts.js
+++ routes/posts.js
@@ -3,14 +3,32 @@ const router = express.Router();
 const Post = require('../assets/models/target');
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
+const https = require('https');
+const fs = require('fs');
 
-router.post('/', upload.single('file'), async (req,res) => {
+function saveImg(data, path){
+  var dataString = data.split(",", 2);
+  var img64 = dataString[1];
+  var buffer = Buffer.from(img64, "base64");
+  fs.writeFileSync(path, buffer, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("created file success");
+    }
+  });
+}
+
+router.post('/', async (req,res) => {
   console.log(req.body);
+  var path1 = "public/uploads/" + req.body.coords[0] + "_" + req.body.coords[1] + ".png"
+  var path2 = "uploads/" + req.body.coords[0] + "_" + req.body.coords[1] + ".png"
+  saveImg(req.body.img, path1);
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
     coords: req.body.coords,
-    img: req.body.img
+    img: path2
   });
   console.log(post);
   try {
